7b4a78b039b11f6a5588da1cf6c8d200e04942ff
Gabi13st.h <gabs13.h@gmail.com>
2024-05-14 12:11:43 -0400

    Agrega función crear una nueva baraja

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -4,13 +4,34 @@
 // 2H = Two of Hearts 
 // 2S = Two of Spades
 
-let deck = [];
+//Baraja
+let deck        = [];
+let tipos       = ['C', 'D', 'H', 'S'];
+let especiales  = ['A', 'J', 'Q', 'K'];
 
+// Esta función crea una nueva baraja en aleatorio:
 const crearDeck  = () => {
 
     for (let i=2; i <=10; i++){
-        console.log('Hola')
+        for (let tipo of tipos){
+            deck.push(i + tipo)  
+        }
     }
+
+    for (let especial of especiales){
+        for (let tipo of tipos){
+            deck.push(especial + tipo)
+        }
+    }
+    
+    //Esta baraja está en orden. Por lo que para que salga desordenado, 
+    // usaremos una librería: underscore para revolver todas las cartas.
+    deck = _.shuffle(deck);
+
+    return deck;
 }
 
 crearDeck();    
+
+
+// Esta función permite tomar una carta:
