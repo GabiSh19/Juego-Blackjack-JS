91d781a0f3bea6d8a4f61e08b5ecee1d6ac32a5d
Gabi13st.h <gabs13.h@gmail.com>
2024-05-13 22:44:14 -0400

    Avance de lógica de juego

---
+++ assets/js/juego.js
@@ -0,0 +1,16 @@
+
+// 2C = Two of Clubs 
+// 2D = Two of Diamonds 
+// 2H = Two of Hearts 
+// 2S = Two of Spades
+
+let deck = [];
+
+const crearDeck  = () => {
+
+    for (let i=2; i <=10; i++){
+        console.log('Hola')
+    }
+}
+
+crearDeck();    
